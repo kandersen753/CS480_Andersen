493273eac6dfad58a0265424ba5e42fdea96b1d9
kandersen753 <kurt.t.andersen@gmail.com>
2016-09-06 15:12:46 -0700

    updated file paths

--- PA1/include/shader.h
+++ PA1/include/shader.h
@@ -12,7 +12,7 @@ class Shader
     ~Shader();
     bool Initialize();
     void Enable();
-    bool AddShader(GLenum ShaderType);
+    bool AddShader(GLenum ShaderType, std::string fileName);
     bool Finalize();
     GLint GetUniformLocation(const char* pUniformName);
 
--- PA1/src/shader.cpp
+++ PA1/src/shader.cpp
@@ -34,68 +34,22 @@ bool Shader::Initialize()
 }
 
 // Use this method to add shaders to the program. When finished - call finalize()
-bool Shader::AddShader(GLenum ShaderType)
+bool Shader::AddShader(GLenum ShaderType, std::string fileName)
 {
   std::string s;
   char temp[100];
   std::ifstream fin;
 
-  if(ShaderType == GL_VERTEX_SHADER) //file .glsl
+  fin.open(fileName.c_str());
+  while (fin.good())
   {
-    fin.open("../shaders/vertexShader.glsl");
-    while (fin.good())
-    {
-      fin.getline(temp , 100);
-      s += temp;
-      s += '\n';
-    }
-
-    fin.close();
-    fin.clear();
-    /*s = "#version 330\n \
-          \
-          layout (location = 0) in vec3 v_position; \
-          layout (location = 1) in vec3 v_color; \
-          \
-          smooth out vec3 color; \
-          \
-          uniform mat4 projectionMatrix; \
-          uniform mat4 viewMatrix; \
-          uniform mat4 modelMatrix; \
-          \
-          void main(void) \
-          { \
-            vec4 v = vec4(v_position, 1.0); \
-            gl_Position = (projectionMatrix * viewMatrix * modelMatrix) * v; \
-            color = v_color; \
-          } \
-          ";*/
+    fin.getline(temp , 100);
+    s += temp;
+    s += '\n';
   }
-  else if(ShaderType == GL_FRAGMENT_SHADER)
-  {
 
-    fin.open("../shaders/fragmentShader.glsl");
-    while (fin.good())
-    {
-      fin.getline(temp , 100);
-      s += temp;
-      s += '\n';
-    }
-
-    fin.close();
-    fin.clear();
-    /*s = "#version 330\n \
-          \
-          smooth in vec3 color; \
-          \
-          out vec4 frag_color; \
-          \
-          void main(void) \
-          { \
-             frag_color = vec4(color.rgb, 1.0); \
-          } \
-          ";*/
-  }
+  fin.close();
+  fin.clear();
 
   GLuint ShaderObj = glCreateShader(ShaderType);
 
